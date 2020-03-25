9ccb1a41ec698c9301db664ff69f6db1f4ff3eab
Adriano Cucci <cucciadriano@gmail.com>
2020-03-25 04:18:33 -0400

    Replaced home route with root route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { GalleryPage } from './pages/gallery/gallery.page';
 import { PageNotFoundPage } from './pages/page-not-found/page-not-found.page';
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
-  { path: "home", component: HomePage },
+  { path: "home", redirectTo: "" },
+  { path: "", component: HomePage },
   { path: "music", component: MusicPage },
   { path: "biography", component: BiographyPage },
   { path: "videos", component: VideosPage },
