7189b1d228b889f19034211ab0aead207351c851
Adriano Cucci <cucciadriano@gmail.com>
2020-03-29 14:34:21 -0400

    Added new gallery images.

--- src/app/pages/gallery/gallery.page.ts
+++ src/app/pages/gallery/gallery.page.ts
@@ -26,56 +26,92 @@ export class GalleryPage extends Page {
             alt: "person-looking-away",
           },
           {
-            src: "assets/images/photos/gallery/posing-1.jpg",
-            alt: "person-posing",
-          },
-          {
             src: "assets/images/photos/gallery/looking-away-1.jpg",
             alt: "person-looking-away",
-          }
-        ]
-      },
-      {
-        rowImages: [
+          },
           {
             src: "assets/images/photos/gallery/holding-mirror.jpg",
             alt: "person-holding-mirror",
-          },
-          {
-            src: "assets/images/photos/gallery/posing-2.jpg",
-            alt: "person-posing",
-          },
-          {
-            src: "assets/images/photos/gallery/holding-phone-cord.jpg",
-            alt: "person-holding-phone-cord",
           }
         ]
       },
       {
         rowImages: [
           {
-            src: "assets/images/photos/gallery/posing-with-phone.jpg",
-            alt: "person-posing-with-phone",
+            src: "assets/images/photos/gallery/holding-phone-cord.jpg",
+            alt: "person-holding-phone-cord",
           },
           {
             src: "assets/images/photos/gallery/sitting-with-phone-1.jpg",
             alt: "person-sitting-with-phone",
           },
           {
-            src: "assets/images/photos/gallery/sitting-with-phone-2.jpg",
-            alt: "person-sitting-with-phone",
+            src: "assets/images/photos/gallery/posing-with-phone.jpg",
+            alt: "person-posing-with-phone",
           }
         ]
       },
       {
         rowImages: [
           {
-            src: "assets/images/photos/gallery/talking-on-phone-1.jpg",
-            alt: "person-talking-on-phone",
+            src: "assets/images/photos/gallery/sitting-with-phone-2.jpg",
+            alt: "person-sitting-with-phone",
           },
           {
             src: "assets/images/photos/gallery/talking-on-phone-2.jpg",
             alt: "person-talking-on-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/talking-on-phone-1.jpg",
+            alt: "person-talking-on-phone",
+          }
+        ]
+      },
+      {
+        rowImages: [
+          {
+            src: "assets/images/photos/gallery/laying-with-phone-1.jpg",
+            alt: "person-laying-with-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/sitting-with-phone-3.jpg",
+            alt: "person-sitting-with-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/standing-with-phone-1.jpg",
+            alt: "person-standing-with-phone",
+          }
+        ]
+      },
+      {
+        rowImages: [
+          {
+            src: "assets/images/photos/gallery/sitting-with-phone-4.jpg",
+            alt: "person-sitting-with-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/standing-with-phone-2.jpg",
+            alt: "person-standing-with-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/laying-with-phone-2.jpg",
+            alt: "person-laying-with-phone",
+          }
+        ]
+      },
+      {
+        rowImages: [
+          {
+            src: "assets/images/photos/gallery/standing-with-phone-3.jpg",
+            alt: "person-standing-with-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/laying-with-phone-3.jpg",
+            alt: "person-laying-with-phone",
+          },
+          {
+            src: "assets/images/photos/gallery/sitting-with-phone-5.jpg",
+            alt: "person-sitting-with-phone",
           }
         ]
       }
