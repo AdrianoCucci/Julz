110a8696eeef249b6663605be6c1b8534b9a7a3b
Adriano Cucci <cucciadriano@gmail.com>
2020-03-30 22:14:22 -0400

    Removed extra spaces.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
-
 @NgModule({
   imports: [RouterModule.forRoot(environment.routes, {
     initialNavigation: 'enabled'
