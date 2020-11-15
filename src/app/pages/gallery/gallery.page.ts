fe23d773b4b2dffdd37276ece2eeb156eb6633dd
Adriano Cucci <cucciadriano@gmail.com>
2020-11-15 13:57:26 -0500

    Rearranged gallary images.

--- src/app/pages/gallery/gallery.page.ts
+++ src/app/pages/gallery/gallery.page.ts
@@ -35,12 +35,12 @@ export class GalleryPage extends Page {
         rowImages: [
           { src: "assets/images/photos/gallery/laying-with-phone-3.jpg", alt: "person-laying-with-phone", },
           { src: "assets/images/photos/gallery/holding-mirror.jpg", alt: "person-holding-mirror", },
-          { src: "assets/images/photos/gallery/sitting-with-phone-3.jpg", alt: "person-sitting-with-phone", }
+          { src: "assets/images/photos/gallery/standing-with-glitter-2.jpeg", alt: "person-standing-with-glitter-background", }
         ]
       },
       {
         rowImages: [
-          { src: "assets/images/photos/gallery/looking-away-2.jpg", alt: "person-looking-away", },
+          { src: "assets/images/photos/gallery/standing-with-glitter-4.jpeg", alt: "person-standing-with-glitter-background", },
           { src: "assets/images/photos/gallery/sitting-with-phone-2.jpg", alt: "person-sitting-with-phone", },
           { src: "assets/images/photos/gallery/laying-with-phone-1.jpg", alt: "person-laying-with-phone", }
         ]
@@ -62,28 +62,28 @@ export class GalleryPage extends Page {
       {
         rowImages: [
           { src: "assets/images/photos/gallery/talking-on-phone-2.jpg", alt: "person-talking-on-phone", },
-          { src: "assets/images/photos/gallery/posing-with-phone.jpg", alt: "person-posing-with-phone", },
+          { src: "assets/images/photos/gallery/sitting-1.jpg", alt: "person-sitting", },
           { src: "assets/images/photos/gallery/standing-with-phone-1.jpg", alt: "person-standing-with-phone", }
         ]
       },
       {
         rowImages: [
           { src: "assets/images/photos/gallery/standing-with-glitter-1.jpeg", alt: "person-standing-with-glitter-background", },
-          { src: "assets/images/photos/gallery/standing-with-glitter-2.jpeg", alt: "person-standing-with-glitter-background", },
+          { src: "assets/images/photos/gallery/sitting-2.jpg", alt: "person-sitting", },
           { src: "assets/images/photos/gallery/standing-with-glitter-3.jpeg", alt: "person-standing-with-glitter-background", },
         ]
       },
       {
         rowImages: [
-          { src: "assets/images/photos/gallery/standing-with-glitter-4.jpeg", alt: "person-standing-with-glitter-background", },
+          { src: "assets/images/photos/gallery/looking-away-2.jpg", alt: "person-looking-away", },
           { src: "assets/images/photos/gallery/standing-with-glitter-5.jpg", alt: "person-standing-with-glitter-background", },
           { src: "assets/images/photos/gallery/looking-in-mirror.jpg", alt: "person-looking-in-mirror", },
         ]
       },
       {
         rowImages: [
-          { src: "assets/images/photos/gallery/sitting-1.jpg", alt: "person-sitting", },
-          { src: "assets/images/photos/gallery/sitting-2.jpg", alt: "person-sitting", }
+          { src: "assets/images/photos/gallery/posing-with-phone.jpg", alt: "person-posing-with-phone", },
+          { src: "assets/images/photos/gallery/sitting-with-phone-3.jpg", alt: "person-sitting-with-phone", }
         ]
       }
     ];
