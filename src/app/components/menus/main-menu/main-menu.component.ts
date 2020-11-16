0e7c9a93eb91bc0364a3d1acf965188d0fe0b827
Adriano Cucci <cucciadriano@gmail.com>
2020-11-15 20:38:58 -0500

    Added main menu link icons and link to merch store.

--- src/app/components/menus/main-menu/main-menu.component.ts
+++ src/app/components/menus/main-menu/main-menu.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { faHome, faMusic, faStar, faImages, faVideo, faShoppingBag, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
   styleUrls: ['./main-menu.component.scss']
 })
-export class MainMenuComponent { }
+export class MainMenuComponent { 
+  public readonly iconHome: IconDefinition = faHome;
+  public readonly iconMusic: IconDefinition = faMusic;
+  public readonly iconAbout: IconDefinition = faStar;
+  public readonly iconVideos: IconDefinition = faVideo;
+  public readonly iconGallery: IconDefinition = faImages;
+  public readonly iconMerch: IconDefinition = faShoppingBag;
+}
